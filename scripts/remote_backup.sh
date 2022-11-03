eb2737795a8748c59d4ef92e0f3e888e8652ad14
harrypulvirenti <harrypulvirenti@gmail.com>
2022-11-03 19:12:37 +0100

    Test Thu  3 Nov 19:12:37 CET 2022

--- scripts/remote_backup.sh
+++ scripts/remote_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git -C ~/klipper_config pull
 git -C ~/klipper_config add .
-git -C ~/klipper_config commit -m "`date`"
+git -C ~/klipper_config commit -m "$1 `date`"
 git -C ~/klipper_config push
